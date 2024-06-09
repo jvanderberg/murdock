234390a4a0f849c2bcb36f37beb25d271375f81e
Josh Vanderberg <jvanderberg@gmail.com>
2024-06-08 22:07:53 -0400

    Attempt to get .d.ts right, bump

--- murdock-custom-element/vite.config.ts
+++ murdock-custom-element/vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
-
+import dts from 'vite-plugin-dts';
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
+	plugins: [
+		preact(),
+		dts({
+			copyDtsFiles: true
+		})
+	],
 	build: {
 		target: 'esnext',
 		lib: {
